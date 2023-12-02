b8d27d32f2604ca704f865bf9bfcd3e59aa1b56b
moawezz <moawez.de@gmail.com>
2023-12-02 16:02:12 +0330

    text formatting

--- test.go
+++ test.go
@@ -2,6 +2,10 @@ package main
 
 import "fmt"
 
+const secondsInHour = 3600
+
 func main() {
-	fmt.Println("Hello go.")
+	distance := 60.8
+	fmt.Printf("Distance in mile: %f \n", distance*0.621)
+
 }
