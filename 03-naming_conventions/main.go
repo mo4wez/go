efd6ff86e375be32687f9feec5967777c6329cae
moawezz <moawez.de@gmail.com>
2023-12-03 10:38:58 +0330

    complete nameing conventions

--- 03-naming_conventions/main.go
+++ 03-naming_conventions/main.go
@@ -2,6 +2,8 @@ package main
 
 import "fmt"
 
+var taskDone bool
+
 func main() {
 	fmt.Println("Naming Conventions")
 
@@ -9,4 +11,18 @@ func main() {
 	var mv int // max value
 	_ = mv
 	// var max_value int // Not Ok
+
+	// in small scope
+	// var packegesReceived int // Not Ok, too long
+	// var b int // Good practice
+
+	// const MAX_VALUE = 100 // Not Ok
+	// const N = 1000 // better
+
+	// maxValue := 1000 // recommended
+	// max_value := 1000 // not idiometic
+
+	// writeToDB := true // ok, idiometic
+	// writeToDb := true // not idiometic
+
 }
